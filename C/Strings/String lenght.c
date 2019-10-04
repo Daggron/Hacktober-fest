54221d92ceba055f3ae25f74e0e024e3fa29f5f4
samarth <samarth.gugnani30@gmail.com>
2019-10-04 22:16:05 +0530

    pushed some more programmes of strings

---
+++ C/Strings/String lenght.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int i;
+ char s[100];
+ scanf("%s",s);
+ for(i=0;s[i]!='\0';i++){
+
+ }
+
+ printf("the length of the string is %d",i);
+}
