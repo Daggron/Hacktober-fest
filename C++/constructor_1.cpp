64fed9aacfbb6bf00502703f796dcc6b8ecc69f8
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-10-07 13:49:38 +0530

    add constructor

---
+++ C++/constructor_1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+	private: int real,img;
+	public: void read() {
+		cout<<"Enter the num 1"<<endl;
+		cin>>real;
+		cout<<"Enter the num 2"<<endl;
+		cin>>img;
+	}
+	complex(){}
+	complex(int a,int b) {
+		real = a;
+		img = b;
+	}
+	complex(complex &c1, complex &c2) {
+		real = c1.real + c2.real;
+		img = c1.img + c2.img;
+	}
+	void disp() {
+		cout<<real<<" "<<img;
+	}
+};
+int main()
+{
+	complex c1; //default constructor
+	c1.read();
+	complex c2;
+	c2.read();
+	complex c3(c1,c2); // copy constructor
+	c3.disp();
+}
