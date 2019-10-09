77f4c3e98e02dd6644422ac494383ecbdd1488d9
Yashank18 <yashank1050.cse18@chitkara.edu.in>
2019-10-09 15:01:32 +0530

    Updtaed Dart file

--- statefull/lib/main.dart
+++ statefull/lib/main.dart
@@ -7,13 +7,18 @@ void main(){
 class Myapp extends StatelessWidget {
   
 
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: "Statefull App",
-      home: new homepage(),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+      return new MaterialApp(
+        title: "Statefull App",
+        home: new homepage(),
+        theme: new ThemeData(
+          primarySwatch: Colors.green,
+          brightness: Brightness.light, //if use Brightness.dark it turns into dark theme
+          accentColor: Colors.red,
+        ),
+      );
+    }
 }
 
 class homepage extends StatefulWidget {
@@ -28,42 +33,54 @@ class homepage extends StatefulWidget {
 
 class _homepageState extends State<homepage> {
 
-String mytext = "Hello User";
+  String mytext = "Hello User";
 
-void _changeText(){
-setState(() {
-  if(mytext.startsWith("H"))
-  mytext = "Welcome to my App";
-  else
-  mytext = "Hello User";
-});
-}
+  void _changeText(){
+  setState(() {
+    if(mytext.startsWith("H"))
+    mytext = "Welcome to my App";
+    else
+    mytext = "Hello User";
+  });
+  }
 
-Widget _bodyWidget() {
-  return Container(
-    padding: const EdgeInsets.all(8.0),
-    child: new Center(
-      child: new Column(
-        children: <Widget>[
-          new Text("Wanna See Magic?"),
-          new RaisedButton(
-            child: new Text("Click"),
-            onPressed: _changeText,
-          )
-        ],
+  Widget _bodyWidget() {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(mytext,style: new TextStyle(
+              fontSize: 40.0,
+              color: Colors.red,
+            ),),
+            new RaisedButton(
+              child: new Text("Click", style: new TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+              ),),
+              onPressed: _changeText,
+              color: Colors.green,
+            )
+          ],
+        ),
       ),
-    ),
-  );
-}
-
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar:new AppBar(
-        title: new Text("Working"),
-      ),
-      body: new Center(child: new Text("I am not static"),),
     );
   }
+
+
+    @override
+    Widget build(BuildContext context) {
+      return new Scaffold(
+        appBar:new AppBar(
+          title: new Text("Pulkit ka App"),
+        ),
+        body:  _bodyWidget(),
+        floatingActionButton: new FloatingActionButton(
+          child: new Icon(Icons.add),
+          onPressed: _changeText,
+        ),
+      );
+    }
 }
