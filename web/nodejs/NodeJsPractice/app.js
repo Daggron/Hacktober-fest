707394f4cf8400fe3ce251c6d2ce2a093056674d
Daggron <returnofking04@gmail.com>
2019-10-10 14:59:14 +0530

    node js

---
+++ web/nodejs/NodeJsPractice/app.js
@@ -0,0 +1,20 @@
+
+
+//JS objects 
+
+let obj = {
+    name:"Abhay",
+    age:12,
+    array:[
+        {
+            id:1,
+            son:'abc'
+        },
+        {
+            id:2,
+            daughter:'xyz'
+        }
+    ]
+}
+
+console.log(`${obj.array[0].id}`);
