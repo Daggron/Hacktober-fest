957098d4be770a56a7112c0532335a2e86ff997c
Jyoti Singla <56391719+jyotisingla08@users.noreply.github.com>
2020-10-02 14:29:00 +0530

    Update upper case lower case.c

--- C/Strings/upper case lower case.c
+++ C/Strings/upper case lower case.c
@@ -8,6 +8,7 @@ int main()
    scanf("%s",s);
 
    for(i=0;s[i] != '\0';i++){
+      // comparing ascii values for upper and lower case
     if(s[i]>=65 && s[i] <=90){
         s[i] = s[i]+32;
     }else if(s[i] >= 97 && s[i] <=122){
