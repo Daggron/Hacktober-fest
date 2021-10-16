ccf5a085dbc01b79e873231934cb957cb1102a99
Mansi Gulati <54800192+mansigulati@users.noreply.github.com>
2021-10-16 17:07:54 +0530

    added comment for the programme

--- GO/fibonacci.go
+++ GO/fibonacci.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+// Programme to generate the 10 letters fibonnaci series recursively 
+// Sample output will be 
+// 0 1 1 2 3 5 8 13 21 34 
+
 func fibonaci(i int) (ret int) {
    if i == 0 {
       return 0
